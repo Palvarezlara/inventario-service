6bf82ab13d33958153212ef2fc6efb3658bc9820
PalvarezLara <pame.alvarez@duocuc.cl>
2025-05-13 13:25:36 -0400

    Avance: data.sql sin acentos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,17 @@
 
 DELETE FROM producto;
 
+-- Script para poblar la tabla 'producto'
+
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Floral', 10, 19990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Amaderado', 5, 25990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Cítrico', 8, 14990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Dulce', 12, 17990.0);
-INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Fresco', 7, 21990.0); 
+INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Fresco', 7, 21990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Oriental', 3, 29990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Verde', 6, 23990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume Deportivo', 4, 15990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume de Noche', 2, 24990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume de Día', 9, 18990.0);
-INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume de Verano', 11, 20990.0); 
+INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume de Verano', 11, 20990.0);
 INSERT INTO producto (nombre, stock, precio) VALUES ('Perfume de Invierno', 1, 26990.0);
