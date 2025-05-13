7d8cc9b56726775cf78ea7e81938ba6087f9ade4
PalvarezLara <pame.alvarez@duocuc.cl>
2025-05-13 13:23:00 -0400

    Avance: cambio de oracle a MySQL

--- src/main/java/com/perfulandia/inventario/InventarioServiceApplication.java
+++ src/main/java/com/perfulandia/inventario/InventarioServiceApplication.java
@@ -1,12 +1,9 @@
 package com.perfulandia.inventario;
 
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import com.perfulandia.inventario.model.Producto;
-import com.perfulandia.inventario.repository.ProductoRepository;
 
 @SpringBootApplication
 public class InventarioServiceApplication {
@@ -15,18 +12,6 @@ public class InventarioServiceApplication {
 		SpringApplication.run(InventarioServiceApplication.class, args);
 	}
 
-	// Aquí puedes agregar más configuraciones o beans si es necesario
-	@Bean
-	public CommandLineRunner demo(ProductoRepository productoRepository) {
-		return args -> {
-			Producto p = new Producto();
-            p.setNombre("Aceite de masaje");
-            p.setStock(25);
-            p.setPrecio(4990.0);
-
-            productoRepository.save(p);
-            System.out.println("✅ Producto guardado correctamente."
-			);
-		};
-	}
+	
+	
 }
--- src/main/java/com/perfulandia/inventario/model/Producto.java
+++ src/main/java/com/perfulandia/inventario/model/Producto.java
@@ -2,7 +2,6 @@ package com.perfulandia.inventario.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,13 +10,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "producto")
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Producto {
    @Id
-   @GeneratedValue
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
    @Column(nullable = false)
